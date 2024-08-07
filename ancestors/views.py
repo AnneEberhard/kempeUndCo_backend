a02432414feba5cc65ce7562ef7dc08ffe4f34d5
AnneEberhard <anne.eberhard@gmx.net>
2024-08-07 09:34:16 +0200

    added urls and views for persons, discussions, relations. Discussion not tested

--- ancestors/views.py
+++ ancestors/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Person, Relation
+from .serializers import PersonSerializer, RelationSerializer
 
-# Create your views here.
+class PersonListCreateView(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class PersonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+class RelationListCreateView(generics.ListCreateAPIView):
+    queryset = Relation.objects.all()
+    serializer_class = RelationSerializer
+
+class RelationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Relation.objects.all()
+    serializer_class = RelationSerializer
+    lookup_field = 'person_id'  # Verwende person_id als Lookup-Feld
+
+    def get_queryset(self):
+        return Relation.objects.filter(person_id=self.kwargs['person_id'])
+    
