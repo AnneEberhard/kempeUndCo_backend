bc4305f8081c5baa171159a1a05af25a4fbcdca4
AnneEberhard <anne.eberhard@gmx.net>
2024-08-16 15:10:11 +0200

    refined serializer to minimize data transfer

--- ancestors/serializers.py
+++ ancestors/serializers.py
@@ -5,7 +5,53 @@ from .models import Person, Relation
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = '__all__'  # oder liste spezifische Felder auf, z.B. ['id', 'name', 'refn', ...]
+        fields = [
+            'id',
+            'refn',
+            'name',
+            'surn',
+            'givn',
+            'sex',
+            'occu',
+            'chan_date',
+            'chan_date_time',
+            'birt_date',
+            'birth_date_formatted',
+            'birt_plac',
+            'deat_date',
+            'death_date_formatted',
+            'deat_plac',
+            'note',
+            'chr_date',
+            'chr_plac',
+            'buri_date',
+            'buri_plac',
+            'name_rufname',
+            'name_npfx',
+            'sour',
+            'name_nick',
+            'name_marnm',
+            'chr_addr',
+            'reli',
+            'obje_file_1',
+            'obje_file_2',
+            'obje_file_3',
+            'obje_file_4',
+            'obje_file_5',
+            'obje_file_6',
+            'confidential'
+        ]
+
+
+class PersonListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = [
+            'id',
+            'name',
+            'surn',
+            'givn'
+        ]
 
 
 class RelationSerializer(serializers.ModelSerializer):
