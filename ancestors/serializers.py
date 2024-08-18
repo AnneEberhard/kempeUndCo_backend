93e40169eb711dccd376392525f369b2ab03500e
AnneEberhard <anne.eberhard@gmx.net>
2024-08-18 10:06:21 +0200

    adapted serializer with confidential levels

--- ancestors/serializers.py
+++ ancestors/serializers.py
@@ -42,6 +42,81 @@ class PersonSerializer(serializers.ModelSerializer):
             'confidential'
         ]
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.confidential == 'yes':
+            return {
+                'id': instance.id,
+                'name': 'vertraulich',
+                'surn': '',
+                'givn': '',
+                'sex': '',
+                'occu': '',
+                'chan_date': '',
+                'chan_date_time': '',
+                'birt_date': '',
+                'birth_date_formatted': '',
+                'birt_plac': '',
+                'deat_date': '',
+                'death_date_formatted': '',
+                'deat_plac': '',
+                'note': '',
+                'chr_date': '',
+                'chr_plac': '',
+                'buri_date': '',
+                'buri_plac': '',
+                'name_rufname': '',
+                'name_npfx': '',
+                'sour': '',
+                'name_nick': '',
+                'name_marnm': '',
+                'chr_addr': '',
+                'reli': '',
+                'obje_file_1': '',
+                'obje_file_2': '',
+                'obje_file_3': '',
+                'obje_file_4': '',
+                'obje_file_5': '',
+                'obje_file_6': '',
+                'confidential': instance.confidential
+            }
+        elif instance.confidential == 'restricted':
+            return {
+                'id': instance.id,
+                'name': instance.name,
+                'surn': '',
+                'givn': '',
+                'sex': '',
+                'occu': '',
+                'chan_date': '',
+                'chan_date_time': '',
+                'birt_date': '',
+                'birth_date_formatted': '',
+                'birt_plac': '',
+                'deat_date': '',
+                'death_date_formatted': '',
+                'deat_plac': '',
+                'note': '',
+                'chr_date': '',
+                'chr_plac': '',
+                'buri_date': '',
+                'buri_plac': '',
+                'name_rufname': '',
+                'name_npfx': '',
+                'sour': '',
+                'name_nick': '',
+                'name_marnm': '',
+                'chr_addr': '',
+                'reli': '',
+                'obje_file_1': '',
+                'obje_file_2': '',
+                'obje_file_3': '',
+                'obje_file_4': '',
+                'obje_file_5': '',
+                'obje_file_6': '',
+                'confidential': instance.confidential
+            }
+        return representation
 
 class PersonListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -52,6 +127,24 @@ class PersonListSerializer(serializers.ModelSerializer):
             'surn',
             'givn'
         ]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.confidential == 'yes':
+            return {
+                'id': instance.id,
+                'name': 'vertraulich',
+                'surn': '',
+                'givn': ''
+            }
+        elif instance.confidential == 'restricted':
+            return {
+                'id': instance.id,
+                'name': instance.name,
+                'surn': '',
+                'givn': ''
+            }
+        return representation
 
 
 class RelationSerializer(serializers.ModelSerializer):
