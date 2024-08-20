a3a25ba1186b05ca4f454e725d9835467f1a4ab5
AnneEberhard <anne.eberhard@gmx.net>
2024-08-20 17:33:10 +0200

    discussion backend works

--- discussions/admin.py
+++ discussions/admin.py
@@ -10,6 +10,21 @@ class DiscussionEntryInline(admin.TabularInline):
 class DiscussionPageAdmin(admin.ModelAdmin):
     inlines = [DiscussionEntryInline]
 
+    list_display = ('id', 'discussion_for', 'created_at', 'updated_at')
+
+    def discussion_for(self, obj):
+        return f"{obj.person.id} - {obj.person.name}"
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+    
+
+class DiscussionEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'discussion_for', 'author', 'created_at', 'updated_at')
+
+    def discussion_for(self, obj):
+        return f"{obj.discussion.person.id} - {obj.discussion.person.name}"
+
 
 admin.site.register(Discussion, DiscussionPageAdmin)
-admin.site.register(DiscussionEntry)
+admin.site.register(DiscussionEntry, DiscussionEntryAdmin)
--- discussions/urls.py
+++ discussions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateDiscussionEntryView, get_all_discussions, get_or_create_discussion
+from .views import CreateDiscussionEntryView, DiscussionEntryDetailView, get_all_discussions, get_or_create_discussion
 
 
 urlpatterns = [
     path('entries/', CreateDiscussionEntryView.as_view(), name='create_discussion_entry'),
+    path('entries/<int:pk>/', DiscussionEntryDetailView.as_view(), name='discussion_entry_detail'),
     path('<str:id>/', get_or_create_discussion, name='get_or_create_discussion'),
     path('', get_all_discussions, name='get_all_discussions'),
     
