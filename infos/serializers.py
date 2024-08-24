241ef3c4f2935e35c18772881619a1ec254f5a57
AnneEberhard <anne.eberhard@gmx.net>
2024-08-24 09:17:39 +0200

    changed serializer for author email

--- infos/serializers.py
+++ infos/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Info
 
 class InfoSerializer(serializers.ModelSerializer):
+    author_email = serializers.EmailField(source='author.email', read_only=True)
+    
     class Meta:
         model = Info
-        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'image_1', 'image_2', 'image_3', 'image_4']
+        fields = ['id', 'title', 'content', 'author_email', 'created_at', 'updated_at', 'image_1', 'image_2', 'image_3', 'image_4']
         read_only_fields = ['author', 'created_at', 'updated_at']
