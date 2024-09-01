60d0139fbf31ee4c07963e68e53ad8a04baf2c66
AnneEberhard <anne.eberhard@gmx.net>
2024-09-01 18:42:11 +0200

    info updated with familiy selection

--- recipes/admin.py
+++ recipes/admin.py
@@ -7,6 +7,7 @@ from recipes.models import Recipe
 class RecipeAdmin(ImportExportModelAdmin):
     model = Recipe
     list_display = ('id', 'title', 'author')
+    exclude = ('image_1_thumbnail', 'image_2_thumbnail', 'image_3_thumbnail', 'image_4_thumbnail')
 
 
 admin.site.register(Recipe, RecipeAdmin)
