03dd43cc50338d328728a23f91d6b50e523a56e6
AnneEberhard <anne.eberhard@gmx.net>
2024-10-03 12:01:17 +0200

    added personal alerts for comment, info, recipe and discussion entry

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,7 +1,10 @@
 import os
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
+from kempeUndCo_backend.settings import EMAIL_HOST_USER
 from recipes.models import Recipe
 
 
@@ -19,3 +22,15 @@ def delete_images_on_recipe_delete(sender, instance, **kwargs):
         thumbnail = getattr(instance, thumbnail_field)
         if thumbnail and os.path.isfile(thumbnail.path):
             os.remove(thumbnail.path)
+
+
+@receiver(post_save, sender=Recipe)
+def notify_new_recipe(sender, instance, created, **kwargs):
+    if created:
+        send_mail(
+            'Neues Rezept erstellt',
+            f'Es wurde ein neues Rezept mit dem Titel "{instance.title}" auf der Webseite KempeUndCo erstellt.',
+            settings.DEFAULT_FROM_EMAIL,
+            [EMAIL_HOST_USER],
+            fail_silently=False,
+        )
