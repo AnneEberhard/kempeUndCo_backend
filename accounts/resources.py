4fe28aa4d2c17164d8c6c6cd8da3ae439af1b12d
AnneEberhard <anne.eberhard@gmx.net>
2026-07-26 09:30:04 +0200

    added authorname to custom User export

--- accounts/resources.py
+++ accounts/resources.py
@@ -4,5 +4,5 @@ from import_export import resources
 class CustomUserResource(resources.ModelResource):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
+        fields = ('id', 'authorname', 'username', 'email', 'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
         export_order = ('id', 'username', 'email', 'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
