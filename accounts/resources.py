6e169e50a674bf5a19408fc85e59e1be088ff645
AnneEberhard <anne.eberhard@gmx.net>
2026-07-26 10:16:47 +0200

    import id user to email

--- accounts/resources.py
+++ accounts/resources.py
@@ -4,5 +4,7 @@ from import_export import resources
 class CustomUserResource(resources.ModelResource):
     class Meta:
         model = CustomUser
+        import_id_fields = ('email',)
+
         fields = ('id', 'username', 'email','author_name',  'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
         export_order = ('id', 'username', 'email', 'author_name', 'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
