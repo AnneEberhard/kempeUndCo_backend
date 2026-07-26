78256b509d14544a57cbcf6bbae3332a12010648
AnneEberhard <anne.eberhard@gmx.net>
2026-07-26 10:54:36 +0200

    fixed import export user - import not using id but email

--- accounts/resources.py
+++ accounts/resources.py
@@ -2,9 +2,9 @@ from accounts.models import CustomUser
 from import_export import resources
 
 class CustomUserResource(resources.ModelResource):
+
     class Meta:
         model = CustomUser
         import_id_fields = ('email',)
-
-        fields = ('id', 'username', 'email','author_name',  'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
+        fields = ('username', 'email','author_name',  'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
         export_order = ('id', 'username', 'email', 'author_name', 'family_1', 'family_2', 'guarantor', 'guarantor_email', 'notes')
