ff3a41df25562ba2c040fffe22baac9bd2def9ef
AnneEberhard <anne.eberhard@gmx.net>
2026-08-07 17:22:22 +0200

    signals back to email

--- recipes/signals.py
+++ recipes/signals.py
@@ -78,6 +78,7 @@ def notify_new_recipe(sender, instance, created, **kwargs):
                 'user.first_name': user.first_name
             })
             text_content = strip_tags(html_content)
+            print('email')
             email = EmailMultiAlternatives(
                 subject='Neues Rezept erstellt',
                 body=text_content,
@@ -85,7 +86,7 @@ def notify_new_recipe(sender, instance, created, **kwargs):
                 to=[user.email],
             )
             email.attach_alternative(html_content, "text/html")
-            #email.send()
+            email.send()
 
 
           # send_mail(
